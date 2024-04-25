ab341a924ad237655ca72894cfd3c50016838c22
qbh <1328738411@qq.com>
2024-04-25 04:35:15 -0700

    1

--- go.mod
+++ go.mod
@@ -1,33 +1,17 @@
-<<<<<<< HEAD
-module github.com/QinYuuuu/avid-d
+module TMAABE
 
 go 1.20
 
 require github.com/klauspost/reedsolomon v1.12.1
 
 require (
-	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-=======
-module TMAABE
-
-go 1.20
-
-require (
-	github.com/Nik-U/pbc v0.0.0-20181205041846-3e516ca0c5d6
-	github.com/klauspost/reedsolomon v1.12.0
-	github.com/stretchr/testify v1.8.4
-)
-
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/klauspost/cpuid/v2 v2.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.dedis.ch/fixbuf v1.0.3 // indirect
 	go.dedis.ch/kyber/v3 v3.1.0 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
+)
+
+require (
+	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
 	golang.org/x/sys v0.19.0 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
->>>>>>> e982a5d3560d233384b7cc8b8a3b52c93986a5ee
+	google.golang.org/protobuf v1.33.0
 )
--- network/peer_outgoing.go
+++ network/peer_outgoing.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package network
 
 import (
@@ -55,7 +54,7 @@ func (p *outgoingPeer) init() {
 			q.Add("addr", p.ourAddr)
 			req.URL.RawQuery = q.Encode()
 			req.Close = true
-			req.Header.Add("Content-Type", "application/json")// URL encode
+			req.Header.Add("Content-Type", "application/json") // URL encode
 			for {
 				//log.Printf("[node %d] trying connect to node %d on %s", p.ourID, id, ip)
 				resp, err := Client.Do(req)
@@ -126,132 +125,3 @@ func (p *outgoingPeer) SendPost(id int, dataType, api string, data []byte) ([]by
 	defer resp.Body.Close()
 	return respBytes, nil
 }
-=======
-package network
-
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"net/url"
-	"strings"
-	"sync"
-	"time"
-)
-
-type outgoingPeer struct {
-	ourID      int
-	ourAddr    string
-	num        int
-	peers      []peer
-	mutex      *sync.RWMutex
-	ready      bool
-	totalBytes int
-	maxBytes   int
-}
-
-func NewOutgoingPeer(id, num int, addr string, peers []peer, mutex *sync.RWMutex) *outgoingPeer {
-	p := &outgoingPeer{
-		ourID:      id,
-		ourAddr:    addr,
-		num:        num,
-		peers:      peers,
-		mutex:      mutex,
-		ready:      false,
-		totalBytes: 0,
-		maxBytes:   0,
-	}
-	return p
-}
-
-func (p *outgoingPeer) init() {
-	var wg sync.WaitGroup
-	wg.Add(p.num - 1)
-	for _, peer := range p.peers {
-		if p.ourID == peer.Id {
-			continue
-		}
-		go func(ip string, id int) {
-			Client := &http.Client{
-				Timeout: 10 * time.Second,
-			}
-			apiUrl := fmt.Sprintf("http://%s/ready", ip)
-			req, _ := http.NewRequest("GET", apiUrl, nil)
-			q := req.URL.Query()
-			q.Add("id", fmt.Sprint(p.ourID))
-			q.Add("addr", p.ourAddr)
-			req.URL.RawQuery = q.Encode()
-			req.Close = true
-			req.Header.Add("Content-Type", "application/json")// URL encode
-			for {
-				//log.Printf("[node %d] trying connect to node %d on %s", p.ourID, id, ip)
-				resp, err := Client.Do(req)
-				if err != nil {
-					//log.Printf("[node %d] failed connect to node %d on %s\n", p.ourID, id, ip)
-					time.Sleep(10 * time.Second)
-					continue
-				} else {
-					//log.Printf("[node %d] connect to node %d on %s: %s\n", p.ourID, id, ip, resp.Status)
-					wg.Done()
-					resp.Body.Close()
-					break
-				}
-			}
-		}(peer.Addr, peer.Id)
-	}
-	wg.Wait()
-	p.ready = true
-	time.Sleep(1 * time.Second)
-	log.Printf("[node %d] dial ready\n", p.ourID)
-}
-
-func (p *outgoingPeer) SendPost(id int, dataType, api string, data []byte) ([]byte, error) {
-	var ip string
-	Client := &http.Client{
-		Timeout: 30 * time.Second,
-	}
-	for _, peer := range p.peers {
-		if id == peer.Id {
-			ip = peer.Addr
-			break
-		}
-	}
-	apiUrl := fmt.Sprintf("http://%s/%s", ip, api)
-
-	p.mutex.Lock()
-	if len(data) > p.maxBytes {
-		p.maxBytes = len(data)
-	}
-	p.totalBytes += len(data)
-	p.mutex.Unlock()
-
-	postData := url.Values{}
-	postData.Add("From", fmt.Sprintf("%d", p.ourID))
-	postData.Add("Lenth", fmt.Sprintf("%d", len(data)))
-	postData.Add("Type", dataType)
-	postData.Add("Content", string(data))
-	//log.Printf("send message %v", postData)
-	reader := strings.NewReader(postData.Encode())
-	request, err := http.NewRequest("POST", apiUrl, reader)
-	request.Close = true
-	if err != nil {
-		log.Println(err.Error())
-		return nil, err
-	}
-	request.Header.Set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
-	request.Header.Set("Idempotency-Key", fmt.Sprintf("%d%s%d", p.ourID, dataType, id))
-	resp, err := Client.Do(request)
-	if err != nil {
-		//log.Println(err.Error())
-		return nil, err
-	}
-	respBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		//log.Println(err.Error())
-		return nil, err
-	}
-	defer resp.Body.Close()
-	return respBytes, nil
-}
->>>>>>> e982a5d3560d233384b7cc8b8a3b52c93986a5ee
